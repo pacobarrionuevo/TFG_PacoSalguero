bf59c786a56cd2dde15e331ddd51a2fbb0717c8c
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-03-19 11:46:02 +0100

    El registro en teoria va pero hay que tocar el program antes de probarlo

--- TFG_Back/Controllers/UserController.cs
+++ TFG_Back/Controllers/UserController.cs
@@ -1,12 +1,130 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using TFG_Back.Models.Database.Entidades;
+using TFG_Back.Models.Database.Repositorios;
+using TFG_Back.Models.DTO;
+using TFG_Back.Models.Recursos;
 
 namespace TFG_Back.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly DBContext _context;
+        private readonly PasswordHelper passwordHelper;
+        private readonly TokenValidationParameters _tokenParameters;
+        public UserController(DBContext _dbContext, IOptionsMonitor<JwtBearerOptions> jwtOptions)
         {
-            return View();
+            _context = _dbContext;
+            _tokenParameters = jwtOptions.Get(JwtBearerDefaults.AuthenticationScheme).TokenValidationParameters;
+        }
+        private UserSignUpDTO ToDtoRegistro(User users)
+        {
+            return new UserSignUpDTO()
+            {
+                UserNickname = users.UserNickname,
+                UserEmail = users.UserEmail,
+                UserPassword = users.UserPassword,
+                UserConfirmPassword = users.UserConfirmPassword,
+                UserProfilePhoto = null
+            };
+        }
+        private UserDTO ToDto(User users)
+        {
+            return new UserDTO()
+            {
+                UserId = users.UserId,
+                UserNickname = users.UserNickname,
+                UserEmail = users.UserEmail,
+                UserPassword = users.UserPassword,
+                UserConfirmPassword = users.UserConfirmPassword,
+                UserProfilePhoto = users.UserProfilePhoto,
+            
+            };
+        }
+        [HttpGet("users")]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetUsuarios()
+        {
+            return await _context.Users
+                .Select(u => new UserDTO
+                {
+                    UserId = u.UserId,
+                    UserNickname = u.UserNickname,
+                    UserEmail = u.UserEmail,
+                    UserProfilePhoto = u.UserProfilePhoto,
+                    Role = u.Role
+                    
+                })
+                .ToListAsync();
+        }
+        [HttpPost("Registro")]
+        public async Task<IActionResult> Register([FromForm] UserSignUpDTO user)
+        {
+            if (_context.Users.Any(User => User.UserEmail == user.UserEmail))
+            {
+                return BadRequest("El nombre del usuario ya está en uso");
+            }
+
+            if (user.UserPassword!= user.UserConfirmPassword)
+            {
+                return BadRequest("Las contraseñas no coinciden");
+            }
+
+            if (user.UserProfilePhoto == null || user.UserProfilePhoto.Length == 0)
+            {
+                return BadRequest("No se ha elegido foto de perfil");
+            }
+                
+            //Eso guarda la ruta
+            string rutaFotoPerfil = $"{Guid.NewGuid()}_{user.UserProfilePhoto.FileName}";
+            await StoreImageAsync("fotos/" + rutaFotoPerfil, user.UserProfilePhoto);
+
+            User newUser = new User()
+            {
+                UserNickname = user.UserNickname,
+                UserEmail = user.UserEmail,
+                UserPassword = PasswordHelper.Hash(user.UserPassword),
+                UserConfirmPassword = PasswordHelper.Hash(user.UserConfirmPassword),
+                UserProfilePhoto = rutaFotoPerfil,
+                
+            };
+
+            await _context.Users.AddAsync(newUser);
+            await _context.SaveChangesAsync();
+            UserSignUpDTO userCreated = ToDtoRegistro(newUser);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Claims = new Dictionary<string, object>
+                {
+                    {"id", newUser.UserId},
+                    {"Nombre", newUser.UserNickname},
+                    {"Email", newUser.UserEmail},
+                    {"FotoPerfil", newUser.UserProfilePhoto},
+                    {"Rol", newUser.Role},
+                },
+                Expires = DateTime.UtcNow.AddDays(5),
+                SigningCredentials = new SigningCredentials(
+                    _tokenParameters.IssuerSigningKey,
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            string accessToken = tokenHandler.WriteToken(token);
+            return Ok(new { StringToken = accessToken });
+        }
+        private async Task StoreImageAsync(string relativePath, IFormFile file)
+        {
+            using Stream stream = file.OpenReadStream();
+
+            await FileHelper.SaveAsync(stream, relativePath);
         }
     }
+
 }
--- TFG_Back/Models/Database/Repositorios/DbContext.cs
+++ TFG_Back/Models/Database/Repositorios/DbContext.cs
@@ -1,6 +1,32 @@
-﻿namespace TFG_Back.Models.Database.Repositorios
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TFG_Back.Models.Database.Entidades;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TFG_Back.Models.Database.Repositorios
 {
-    public class DbContext
+    public class DBContext : DbContext
     {
+        //Nombre de la base de datos y luego se llama ahi
+        private const string DATABASE_PATH = "Dentista.db";
+
+        //Tablas de la base de datos
+        private readonly Settings _settings;
+        public DbSet<User> Users { get; set; }
+        public DbSet<Entidades.Image> Images { get; set; }
+
+        // Configuramos el EntityFramework para crear un archivo de BBDD Sqlite
+
+        public DBContext(IOptions<Settings> options)
+        {
+            _settings = options.Value;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            options.UseSqlite($"DataSource={baseDir}{DATABASE_PATH}");
+            //options.UseSqlite(_settings.DatabaseConnection);
+        }
     }
 }

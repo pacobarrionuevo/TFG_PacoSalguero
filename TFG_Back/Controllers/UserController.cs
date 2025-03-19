c92b25e76e6e47128c80e00d5a0531aadc926382
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-03-19 09:54:26 +0100

    Algunos archivos añadidos

---
+++ TFG_Back/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TFG_Back.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

b1d12f2ee173fbd6d62c26fa721ae81bd3f70034
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-03-20 10:58:49 +0100

    Estilos basicos menu + botones para cuando este el login metido

--- TFG_Front/src/app/app.routes.ts
+++ TFG_Front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent
+    },
+    {
+        path: 'menu',
+        component: MenuComponent
     }
 ];
--- TFG_Front/src/app/pages/main/main.component.ts
+++ TFG_Front/src/app/pages/main/main.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  isLoggedIn: boolean = false;
+  constructor(private authService: AuthService, private router: Router) {}
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }

a15db625177f1e86098e926fda3be93e0b447376
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-03-20 10:05:14 +0100

    favicon cambiado component login ahora sí creado y app. archivos modificados

--- TFG_Front/src/app/app.component.ts
+++ TFG_Front/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TFG_Front';
+  title = 'SanitariosApp';
 }
--- TFG_Front/src/app/app.routes.ts
+++ TFG_Front/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+];

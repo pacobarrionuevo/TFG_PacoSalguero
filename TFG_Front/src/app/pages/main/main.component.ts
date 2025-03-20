c6d6dfe3c499c4e840f01ddc0b2106d898ac3b58
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-03-20 13:51:16 +0100

    error arreglado de los datos del token

--- TFG_Front/src/app/pages/main/main.component.ts
+++ TFG_Front/src/app/pages/main/main.component.ts
@@ -14,7 +14,14 @@ export class MainComponent {
   isLoggedIn: boolean = false;
   isAdmin: boolean = false;
   constructor(private authService: AuthService, private router: Router) {}
-
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe(loggedIn => {
+      this.isLoggedIn = loggedIn;
+    });
+    this.authService.isAdmin$.subscribe(admin => {
+      this.isAdmin = admin;
+    });
+  }
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);

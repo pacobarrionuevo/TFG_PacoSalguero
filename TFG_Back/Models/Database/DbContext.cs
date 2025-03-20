bc185f5662444a6a03c6550b074dc18697ab8bd6
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-03-20 10:51:23 +0100

    Fondo y comienzo de main simple

--- TFG_Back/Models/Database/DbContext.cs
+++ TFG_Back/Models/Database/DbContext.cs
@@ -8,7 +8,7 @@ namespace TFG_Back.Models.Database
     public class DBContext : DbContext
     {
         // Nombre de la base de datos y luego se llama abajo
-        private const string DATABASE_PATH = "TFG_Sanitarios.db";
+        private const string DATABASE_PATH = "AA_TFG_Sanitarios.db";
 
         // Tablas de la base de datos
         private readonly Settings _settings;

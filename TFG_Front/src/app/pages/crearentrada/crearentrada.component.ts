1c61b4ef7aedd84a9496b7d20aa77cbe91f5d86b
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-04-30 10:30:36 +0200

    componente de crear entrada creado

---
+++ TFG_Front/src/app/pages/crearentrada/crearentrada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crearentrada',
+  standalone: true,
+  imports: [],
+  templateUrl: './crearentrada.component.html',
+  styleUrl: './crearentrada.component.css'
+})
+export class CrearentradaComponent {
+
+}

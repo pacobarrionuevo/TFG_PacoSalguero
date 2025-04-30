077989ad2ac16487c187b631b2b4021081c86452
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-04-30 21:27:35 +0200

    xzcvbn

--- TFG_Front/src/environments/environment.ts
+++ TFG_Front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // Cuando esté subida
-    apiUrl: 'todavía no hay ruta aquí',
+    apiUrl: 'https://tfg-paco-salguero.runasp.net',
     socketUrl: 'todavía no hay una ruta aquí'
 };
   

3f00875496b3e295eec5725d194b809ad0cfb1bd
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-04-30 10:29:29 +0200

    lista de agenda funciona fea pero funciona

--- TFG_Front/src/app/pages/agenda/agenda.component.ts
+++ TFG_Front/src/app/pages/agenda/agenda.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AgendaService } from '../../services/agenda.service';
 import { EntradaAgenda } from '../../models/entrada-agenda';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-agenda',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './agenda.component.html',
   styleUrl: './agenda.component.css'
 })

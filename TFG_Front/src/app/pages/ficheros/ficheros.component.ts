46a5fbba21be4360a3badf033168dcbeba4b0c3b
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-04-30 13:53:24 +0200

    commit antes de irme - seguir

---
+++ TFG_Front/src/app/pages/ficheros/ficheros.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ficheros',
+  standalone: true,
+  imports: [],
+  templateUrl: './ficheros.component.html',
+  styleUrl: './ficheros.component.css'
+})
+export class FicherosComponent {
+
+}

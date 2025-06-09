a3a123d0cf5bb894d5d793715d8f6ce97d44493b
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-06-09 18:02:19 +0200

    nuevoc omponentes creados

---
+++ TFG_Front/src/app/pages/amigos/amigos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amigos',
+  standalone: true,
+  imports: [],
+  templateUrl: './amigos.component.html',
+  styleUrl: './amigos.component.css'
+})
+export class AmigosComponent {
+
+}

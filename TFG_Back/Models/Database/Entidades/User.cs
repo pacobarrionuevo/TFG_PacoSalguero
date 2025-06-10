99ecaf453d916aeba56ecd96223ddee2ecfb03ee
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2025-06-10 10:20:00 +0200

    estilos simples facturas

--- TFG_Back/Models/Database/Entidades/User.cs
+++ TFG_Back/Models/Database/Entidades/User.cs
@@ -1,10 +1,12 @@
-﻿namespace TFG_Back.Models.Database.Entidades
+﻿using TFG_Back.Models.Database.Repositorios;
+
+namespace TFG_Back.Models.Database.Entidades
 {
     public class User
     {
         public int UserId { get; set; }
         public string UserProfilePhoto { get; set; }
-        public string UserNickname{ get; set; }
+        public string UserNickname { get; set; }
         public string UserEmail { get; set; }
         public string UserPassword { get; set; }
         public string UserConfirmPassword { get; set; }
